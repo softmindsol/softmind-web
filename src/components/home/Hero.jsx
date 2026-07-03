79d2c1c9bbcbc4948dc1904594e9e4fa1786e052
Muhammad Huzaifa <softmindsol@gmail.com>
2026-07-03 10:52:47 +0500

    feat: add Hero component with dynamic typewriter heading and call-to-action buttons

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Star } from "lucide-react";
 import { Button } from "../ui/button";
 import { Typewriter } from "../ui/typewriter";
 import CustomButton from "../customs/customButton";
